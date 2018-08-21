dbc8bed4c65594b20cd3b34731e5f1d81027c894
nelsonmarcos <nelsonmarcos@gmail.com>
2018-08-21 08:38:06 -0300

    Improving output format

--- script.py
+++ script.py
@@ -1,14 +1,18 @@
-import time
+import datetime, time
 import http.client as httplib
 
-domains = ['www.google.com', 'www.facebook.com', 'www.amazon.com']
+#Set the domains lists
+#domains = ['www.google.com', 'www.facebook.com', 'www.amazon.com']
+#domains = ['127.0.0.1:8000']
+
 toleration = 0
 
-def is_internet_down(domains, toleration):
+
+def is_internet_down(domains_list, toleration_limit):
     counter = 0
     status = False
     for domain in domains:
-        conn = httplib.HTTPSConnection(domain, timeout=2)
+        conn = httplib.HTTPConnection(domain, timeout=2)
         try:
             conn.request("HEAD", "/")
             conn.close()
@@ -23,14 +27,14 @@ def is_internet_down(domains, toleration):
 
 while True:
     if is_internet_down(domains, toleration):
-        print('\nInternet is down!')
+        time_stamp = time.strftime("%d-%m-%Y %H:%M:%S")
+        print('\n{} - Internet is down!'.format(time_stamp))
         start_time = time.time()
         while is_internet_down(domains, toleration):
             time.sleep(5)
         elapsed_time = time.time() - start_time
-        print ('The internet was down for {} seconds'.format(elapsed_time))
+        time_stamp = time.strftime("%d-%m-%Y %H:%M:%S")
+        print ('{} - The internet was down for {} seconds'.format(time_stamp, int(elapsed_time)))
     else:
         print('.', end='', flush=True)
         time.sleep(1)
-
-
